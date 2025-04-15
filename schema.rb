73ee43b40c211a4d0a4236e85a8177e1acba9946
dfinite-skjung <116854699+dfinite-skjung@users.noreply.github.com>
2025-04-15 17:08:28 +0900

    Update schema.rb

--- schema.rb
+++ schema.rb
@@ -1,167 +1,92 @@
---
--- PostgreSQL database dump
---
+create_table "region", primary_key: "r_regionkey" do |t|
+  t.string "r_name", limit: 25, null: false
+  t.string "r_comment", limit: 152
+end
 
--- Dumped from database version 15.12 (Debian 15.12-1.pgdg120+1)
--- Dumped by pg_dump version 15.12 (Ubuntu 15.12-1.pgdg20.04+1)
+create_table "nation", primary_key: "n_nationkey" do |t|
+  t.string "n_name", limit: 25, null: false
+  t.integer "n_regionkey", null: false
+  t.string "n_comment", limit: 152
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+  t.foreign_key "region", column: "n_regionkey"
+end
 
-SET default_tablespace = '';
+create_table "customer", primary_key: "c_custkey" do |t|
+  t.string "c_name", limit: 25, null: false
+  t.string "c_address", limit: 40, null: false
+  t.integer "c_nationkey", null: false
+  t.string "c_phone", limit: 15, null: false
+  t.float "c_acctbal", null: false
+  t.string "c_mktsegment", limit: 10, null: false
+  t.string "c_comment", limit: 117, null: false
 
-SET default_table_access_method = heap;
+  t.foreign_key "nation", column: "c_nationkey"
+end
 
---
--- Name: customer; Type: TABLE; Schema: public; Owner: postgres
---
+create_table "supplier", primary_key: "s_suppkey" do |t|
+  t.string "s_name", limit: 25, null: false
+  t.string "s_address", limit: 40, null: false
+  t.integer "s_nationkey", null: false
+  t.string "s_phone", limit: 15, null: false
+  t.float "s_acctbal", null: false
+  t.string "s_comment", limit: 101, null: false
 
-CREATE TABLE public.customer (
-    c_custkey bigint NOT NULL,
-    c_name character varying(25) NOT NULL,
-    c_address character varying(40) NOT NULL,
-    c_nationkey integer NOT NULL,
-    c_phone character(15) NOT NULL,
-    c_acctbal double precision NOT NULL,
-    c_mktsegment character(10) NOT NULL,
-    c_comment character varying(117) NOT NULL
-);
+  t.foreign_key "nation", column: "s_nationkey"
+end
 
+create_table "part", primary_key: "p_partkey" do |t|
+  t.string "p_name", limit: 55, null: false
+  t.string "p_mfgr", limit: 25, null: false
+  t.string "p_brand", limit: 10, null: false
+  t.string "p_type", limit: 25, null: false
+  t.integer "p_size", null: false
+  t.string "p_container", limit: 10, null: false
+  t.float "p_retailprice", null: false
+  t.string "p_comment", limit: 23, null: false
+end
 
-ALTER TABLE public.customer OWNER TO postgres;
+create_table "partsupp", primary_key: %w[ps_partkey ps_suppkey] do |t|
+  t.integer "ps_partkey", null: false
+  t.integer "ps_suppkey", null: false
+  t.integer "ps_availqty", null: false
+  t.float "ps_supplycost", null: false
+  t.string "ps_comment", limit: 199, null: false
 
---
--- Name: lineitem; Type: TABLE; Schema: public; Owner: postgres
---
+  t.foreign_key "part", column: "ps_partkey"
+  t.foreign_key "supplier", column: "ps_suppkey"
+end
 
-CREATE TABLE public.lineitem (
-    l_orderkey bigint NOT NULL,
-    l_partkey bigint NOT NULL,
-    l_suppkey bigint NOT NULL,
-    l_linenumber bigint NOT NULL,
-    l_quantity double precision NOT NULL,
-    l_extendedprice double precision NOT NULL,
-    l_discount double precision NOT NULL,
-    l_tax double precision NOT NULL,
-    l_returnflag character(1) NOT NULL,
-    l_linestatus character(1) NOT NULL,
-    l_shipdate date NOT NULL,
-    l_commitdate date NOT NULL,
-    l_receiptdate date NOT NULL,
-    l_shipinstruct character(25) NOT NULL,
-    l_shipmode character(10) NOT NULL,
-    l_comment character varying(44) NOT NULL
-);
+create_table "orders", primary_key: "o_orderkey" do |t|
+  t.integer "o_custkey", null: false
+  t.string "o_orderstatus", limit: 1, null: false
+  t.float "o_totalprice", null: false
+  t.date "o_orderdate", null: false
+  t.string "o_orderpriority", limit: 15, null: false
+  t.string "o_clerk", limit: 15, null: false
+  t.integer "o_shippriority", null: false
+  t.string "o_comment", limit: 79, null: false
 
+  t.foreign_key "customer", column: "o_custkey"
+end
 
-ALTER TABLE public.lineitem OWNER TO postgres;
+create_table "lineitem", primary_key: %w[l_orderkey l_linenumber] do |t|
+  t.integer "l_orderkey", null: false
+  t.integer "l_partkey", null: false
+  t.integer "l_suppkey", null: false
+  t.integer "l_linenumber", null: false
+  t.float "l_quantity", null: false
+  t.float "l_extendedprice", null: false
+  t.float "l_discount", null: false
+  t.float "l_tax", null: false
+  t.string "l_returnflag", limit: 1, null: false
+  t.string "l_linestatus", limit: 1, null: false
+  t.date "l_shipdate", null: false
+  t.date "l_commitdate", null: false
+  t.date "l_receiptdate", null: false
+  t.string "l_shipinstruct", limit: 25, null: false
+  t.string "l_shipmode", limit: 10, null: false
+  t.string "l_comment", limit: 44, null: false
 
---
--- Name: nation; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.nation (
-    n_nationkey integer NOT NULL,
-    n_name character(25) NOT NULL,
-    n_regionkey integer NOT NULL,
-    n_comment character varying(152)
-);
-
-
-ALTER TABLE public.nation OWNER TO postgres;
-
---
--- Name: orders; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.orders (
-    o_orderkey bigint NOT NULL,
-    o_custkey bigint NOT NULL,
-    o_orderstatus character(1) NOT NULL,
-    o_totalprice double precision NOT NULL,
-    o_orderdate date NOT NULL,
-    o_orderpriority character(15) NOT NULL,
-    o_clerk character(15) NOT NULL,
-    o_shippriority integer NOT NULL,
-    o_comment character varying(79) NOT NULL
-);
-
-
-ALTER TABLE public.orders OWNER TO postgres;
-
---
--- Name: part; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.part (
-    p_partkey bigint NOT NULL,
-    p_name character varying(55) NOT NULL,
-    p_mfgr character(25) NOT NULL,
-    p_brand character(10) NOT NULL,
-    p_type character varying(25) NOT NULL,
-    p_size integer NOT NULL,
-    p_container character(10) NOT NULL,
-    p_retailprice double precision NOT NULL,
-    p_comment character varying(23) NOT NULL
-);
-
-
-ALTER TABLE public.part OWNER TO postgres;
-
---
--- Name: partsupp; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.partsupp (
-    ps_partkey bigint NOT NULL,
-    ps_suppkey bigint NOT NULL,
-    ps_availqty bigint NOT NULL,
-    ps_supplycost double precision NOT NULL,
-    ps_comment character varying(199) NOT NULL
-);
-
-
-ALTER TABLE public.partsupp OWNER TO postgres;
-
---
--- Name: region; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.region (
-    r_regionkey integer NOT NULL,
-    r_name character(25) NOT NULL,
-    r_comment character varying(152)
-);
-
-
-ALTER TABLE public.region OWNER TO postgres;
-
---
--- Name: supplier; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.supplier (
-    s_suppkey bigint NOT NULL,
-    s_name character(25) NOT NULL,
-    s_address character varying(40) NOT NULL,
-    s_nationkey integer NOT NULL,
-    s_phone character(15) NOT NULL,
-    s_acctbal double precision NOT NULL,
-    s_comment character varying(101) NOT NULL
-);
-
-
-ALTER TABLE public.supplier OWNER TO postgres;
-
---
--- PostgreSQL database dump complete
---
-
+  t.foreign_key "orders", column: "l_orderkey"
+  t.foreign_key "partsupp", columns: %w[l_partkey l_suppkey]
+end
